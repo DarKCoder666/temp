e4220d704700526c57e0d58dfe4f8bad63ef91fe
Umid <umid010699@mail.ru>
2020-10-06 01:19:25 +0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Router, Route, Switch } from 'react-router-dom'
+import history from './history'
+
+import Home from './components/Home'
+import SignIn from './components/SignIn'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router history={history}>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/signin" component={SignIn} />
+        </Switch>
+      </Router>
     </div>
   );
 }
